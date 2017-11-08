b6d193e699648a24a19719fa08f812c13352c00d
it-elektronika <luka.kavcic@it-elektronika.com>
2017-11-08 11:58:19 +0100

    displaying temp-needs fixing

--- pip.c
+++ pip.c
@@ -3,23 +3,30 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-  SDL_Window* window;
-  SDL_Renderer* renderer;
-  SDL_Texture* back;
-  SDL_Texture* front;
-  
-  TTF_Font* textFont = NULL;
-  SDL_Color textColor = {0, 0, 0, 0};
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Texture* back;
+SDL_Texture* front;
+SDL_Surface *textSurface;
+SDL_Rect renderQuad;	 
+TTF_Font* textFont = NULL;
+SDL_Color textColor = {0, 0, 0, 0};
 
-  int counter = 0;
-  char buff[5];
-  const char* counterText;
-  int textureWidth = 0;
-  int textureHeight = 0;
+int i;
+int counter = 0;
+char buff[5];
+const char* counterText;
+int textureWidth = 0;
+int textureHeight = 0;
+FILE *f1;
+char buf[12];
+char rez[12];
 
 int main(int argc, char *argv[])
 {
-  	
+  i = 0;
+  printf("POINT A\n");
+
   SDL_Init(SDL_INIT_VIDEO);
   if(TTF_Init() == -1)
   {
@@ -30,22 +37,41 @@ int main(int argc, char *argv[])
 
   window = SDL_CreateWindow("PIP", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 400, SDL_WINDOW_SHOWN);
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-
   
+    
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   while(counter < 10000)
   {
-    	  
+    system("vcgencmd measure_temp >> output.txt");
+    system("vcgencmd measure_temp >> output_all.txt");
+    
+    if((f1 = fopen("output.txt", "r")) == NULL)
+    {
+      printf("ERROR OPENING FILE!");
+      exit(2);
+    }
+
+    while((buf[i] = fgetc(f1)) != EOF)
+    {
+      printf("BUF[%d]: %s\n", i, buf);
+      rez[i] = buf[i];
+      i++;
+      if(i+0 > 9)
+      {
+        break;
+      }
+    }
+ //   rez[i] = '\0';
+ //   buf[i] = '\0';
+    printf("REZ: %s\n", rez);
     SDL_RenderClear(renderer);	  
     sprintf(buff, "%d", counter);
-    //buff[5] = '\0'; 
-    SDL_Surface *textSurface;
     SDL_DestroyTexture(front);
     front = NULL;
-    
+    printf("POINT B");
     textureWidth = 0;
     textureHeight = 0;
-    textSurface = TTF_RenderText_Solid(textFont, buff, textColor);
+    textSurface = TTF_RenderText_Solid(textFont, rez, textColor);
     front = SDL_CreateTextureFromSurface(renderer, textSurface);
     SDL_FreeSurface(textSurface);
     if(textSurface == NULL)
@@ -60,7 +86,7 @@ int main(int argc, char *argv[])
     {
       textureWidth = textSurface -> w;
       textureHeight = textSurface -> h;
-      SDL_Rect renderQuad;	    
+     
       renderQuad.x = 320 - (textureWidth/2); 
       renderQuad.y = 200 - (textureHeight/2);
       renderQuad.w = textureWidth;
@@ -70,6 +96,10 @@ int main(int argc, char *argv[])
       SDL_RenderPresent(renderer);
       counter++;
     }
+    printf("POINT C");
+
+    fclose(f1);
+    system("rm output.txt");
   }
   return 0;
 }
